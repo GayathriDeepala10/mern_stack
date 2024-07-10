210c34fe79ee5c99bcc51fe5b494d2951b0033c3
Deepala Gayathri <deepalagaythri137@gmail.com>
2024-07-10 11:56:52 +0530

    some change
    Signed-off-by: Deepala Gayathri <deepalagaythri137@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 import cloudinary from 'cloudinary';
-
+// some change
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
